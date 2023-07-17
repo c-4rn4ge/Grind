30be61d39b3f848b5e893009a02bfc71d81ae5fa
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-07-18 00:58:12 +0530

    Update 0002-add-two-numbers.c

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -6,23 +6,23 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
     int c = 0;
     while(l1!=NULL||l2!=NULL||c)
     {
-            int sum = 0;
-			if (l1 != NULL)
-			{
-				sum += l1->val;
-				l1 = l1->next;
-			}
-			if (l2 != NULL)
-			{
-				sum += l2->val;
-				l2 = l2->next;
-			}
-			sum += c;
-			c = sum / 10; 
-            struct ListNode *t = malloc(sizeof(struct ListNode));
-            t -> val = sum%10;t-> next = NULL;
-            p ->next = t;
-            p = p->next;
+	int sum = 0;
+	if (l1 != NULL)
+	{
+		sum += l1->val;
+		l1 = l1->next;
+	}
+	if (l2 != NULL)
+	{
+		sum += l2->val;
+		l2 = l2->next;
+	}
+	sum += c;
+	c = sum / 10; 
+        struct ListNode *t = malloc(sizeof(struct ListNode));
+        t -> val = sum%10;t-> next = NULL;
+        p ->next = t;
+        p = p->next;
     }
     return l3->next;
 }
