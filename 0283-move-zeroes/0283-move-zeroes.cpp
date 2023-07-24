45a43f5595d06f3f1f84e18ba37d4625e26f6499
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-07-25 04:40:39 +0530

    Time: 24 ms (76.40%), Space: 19.2 MB (30.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) 
     {
-        int i=0 ,j=1;
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int i=0 ,j=1,n = nums.size();
         while(i<nums.size() && j<nums.size())
         {
             if(nums[i]!=0) i++,j++;
