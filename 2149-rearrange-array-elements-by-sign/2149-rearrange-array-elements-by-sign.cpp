38c3a23707c7189b5ffce7eafbafe225c5d96b93
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-07-26 03:40:32 +0530

    Time: 242 ms (75.41%), Space: 123.8 MB (65.68%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) 
     {
-        queue<int> q1,q2;int i;
-        for(i=0;i<nums.size();i++)
+        int i,n=nums.size(),pos=0,neg=1;
+        vector<int> ans(n);
+        for(i=0;i<n;i++)
         {
-            if(nums[i]>0) q1.push(nums[i]);
-            else q2.push(nums[i]);
+            if(nums[i]>0) {ans[pos] = nums[i];pos+=2;}
+            else          {ans[neg] = nums[i];neg+=2;}
         }
-        i=0;
-        while(!q1.empty() || !q2.empty())
-        {
-            if(!q1.empty()) {nums[i++]=q1.front();q1.pop();}
-            if(!q2.empty()){nums[i++]=q2.front();q2.pop();}
-        }
-        return nums;
+        return ans;
     }
 };
