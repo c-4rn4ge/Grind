4a0a91cdd12b8bd5c57e075b1c48c7a1860567c5
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-07-26 07:18:15 +0530

    Time: 18 ms (46.35%), Space: 13.3 MB (32.32%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,44 +2,49 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) 
     {
-        int col = 1,n = mat.size(),m=mat[0].size(),i,j;
-        for(i=0;i<n;i++)
+        int i,j,n=mat.size(),m=mat[0].size(),row=0,col=0;
+        for(i=0;i<n;i++)    if(mat[i][0]==0) row =1;
+        for(i=0;i<m;i++)    if(mat[0][i]==0) col =1;
+        
+        for(i=1;i<n;i++)
         {
-            for(j=0;j<m;j++)
+            for(j=1;j<m;j++)
             {
-                if(mat[i][j]==0) 
+                if(mat[i][j]==0)
                 {
-                    mat[i][0] = 0;
-                    if (j != 0)
-                        mat[0][j] = 0;
-                    else
-                        col = 0;
+                    mat[0][j]=mat[i][0]=0;
                 }
-             }
+            }
         }
+        
+       
         for(i=1;i<n;i++)
         {
             for(j=1;j<m;j++)
             {
                 if(mat[i][j]!=0)
                 {
-                    if(mat[0][j] == 0 ||mat[i][0] == 0)
-                    mat[i][j] = 0;
+                    if(!mat[0][j] || !mat[i][0])
+                    {
+                        mat[i][j]=0;
+                    }
                 }
             }
         }
-        if (mat[0][0] == 0) 
+        
+        if(row)
         {
-            for (j = 0; j < m; j++)
-             {
-                mat[0][j] = 0;
-            }   
-        }
-        if (col == 0) {
-            for ( i = 0; i < n; i++) {
-                mat[i][0] = 0;
+            for(i=0;i<n;i++)
+            {
+                if(mat[i][0]!=0)    mat[i][0]=0;
             }
         }
-
+        if(col)
+        {
+            for(i=0;i<m;i++)
+            {
+                if(mat[0][i]!=0)    mat[0][i]=0;
+            }
+        }
     }
 };
