c49042f46c0b9ea6fb9614bd3f3f011e5cd2a69b
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-03 06:34:00 +0530

    Time: 3 ms (37.56%), Space: 6.5 MB (55.27%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+private:
+    unordered_map<char,string> m{{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
+    void solve(string &d,string c,vector<string> &ans,int i)
+    {
+        if(c.length()==d.length()) ans.push_back(c);
+        else    for(auto j : m[d[i]])   solve(d,c+j,ans,i+1);
+    }
+public:
+    vector<string> letterCombinations(string d) 
+    {
+        if(d.length()==0) return {};
+        vector<string> ans;
+        solve(d,"",ans,0);
+        return ans;
+    }
+};
