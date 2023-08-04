73dcf85a3c9ea98f604b6b612214e71a7f689b35
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-04 08:18:28 +0530

    Time: 832 ms (47.19%), Space: 8 MB (38.82%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,17 +4,36 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
 
         int m = board.size(), n = board[0].size(), k=0;
-        for(int i=0; i<m; i++)  for(int j=0; j<n; j++)  if(board[i][j] == word[k])  if(recurse(board,word,i,j,k))   return true;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(board[i][j] == word[k])
+                {
+                    if(recurse(board,word,i,j,k))
+                       return true;
+                }
+            }
+        }
         return false;
     }
+
     bool recurse(vector<vector<char>>&v, string s, int i, int j, int k)
     {
-        if(i<0||i>=v.size()||j<0||j>=v[0].size()||v[i][j]=='*'||v[i][j]!=s[k])  return false;
-        if(k==s.length()-1) return true;
+        if(i<0||i>=v.size()||j<0||j>=v[0].size()||v[i][j]=='*'||v[i][j]!=s[k])
+           return false;
+    
+        if(k==s.length()-1)
+           return true;
+        
         char ch = v[i][j];
         v[i][j] = '*';
+        
         bool ans = recurse(v,s,i+1,j,k+1) || recurse(v,s,i,j+1,k+1) || recurse(v,s,i-1,j,k+1) || recurse(v,s,i,j-1,k+1);
+
         v[i][j] = ch;
         return ans;
+        
+            
     }
 };
