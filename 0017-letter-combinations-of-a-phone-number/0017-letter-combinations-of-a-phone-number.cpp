1782b885206e91ecca11a570f9bb83a29f2ae9af
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-04 07:44:30 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (55.13%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,18 +1,24 @@
-class Solution 
-{
-private:
-    unordered_map<char,string> m{{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
-    void solve(string &d,string c,vector<string> &ans,int i)
-    {
-        if(c.length()==d.length()) ans.push_back(c);
-        else    {string temp = m[d[i]];for(auto j : temp)   solve(d,c+j,ans,i+1);}
-    }
+class Solution {
 public:
-    vector<string> letterCombinations(string d) 
+    vector<string> dict = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void solve(string &s,string str,vector<string> &ans,int i)
     {
-        if(d.length()==0) return {};
+        if(i==s.length()) 
+        {
+            ans.push_back(str);
+            return;
+        }
+        string temp = dict[s[i]-48];
+        for(int j= 0;j<temp.length();j++)
+        {
+            solve(s,str+temp[j],ans,i+1);
+        }
+    }
+    vector<string> letterCombinations(string s) 
+    {
+        if(s.length()==0) return{};
         vector<string> ans;
-        solve(d,"",ans,0);
+        solve(s,"",ans,0);
         return ans;
     }
 };
