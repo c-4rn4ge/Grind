ab4344c60b4c491e9a07036cd67bb0c7d5b51767
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-04 08:18:07 +0530

    Time: 821 ms (48.04%), Space: 8 MB (70.82%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,26 +1,20 @@
-class Solution 
-{
+class Solution {
+
 public:
-    bool solve(vector<vector<char>>& board,string word,int row,int col)
-    {
-        if( !word.length() ) return true;
-        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[0] ) return false;
-        string s = word.substr(1);
-        char c = board[row][col];
-        board[row][col]='*';
-        bool var = solve(board,s,row,col+1) || solve(board,s,row+1,col) || solve(board,s,row-1,col) || solve(board,s,row,col-1) ;
-        board[row][col]=c;
-        return var;
-    }
-    bool exist(vector<vector<char>>& board, string word) 
-    {    
-        for(int i=0; i<board.size(); i++)
-        {
-            for(int j=0; j<board[i].size(); j++)
-            {
-                if(board[i][j]==word[0])    if(solve(board, word, i, j))    return true;
-            }
-        }
+    bool exist(vector<vector<char>>& board, string word) {
+
+        int m = board.size(), n = board[0].size(), k=0;
+        for(int i=0; i<m; i++)  for(int j=0; j<n; j++)  if(board[i][j] == word[k])  if(recurse(board,word,i,j,k))   return true;
         return false;
+    }
+    bool recurse(vector<vector<char>>&v, string s, int i, int j, int k)
+    {
+        if(i<0||i>=v.size()||j<0||j>=v[0].size()||v[i][j]=='*'||v[i][j]!=s[k])  return false;
+        if(k==s.length()-1) return true;
+        char ch = v[i][j];
+        v[i][j] = '*';
+        bool ans = recurse(v,s,i+1,j,k+1) || recurse(v,s,i,j+1,k+1) || recurse(v,s,i-1,j,k+1) || recurse(v,s,i,j-1,k+1);
+        v[i][j] = ch;
+        return ans;
     }
 };
