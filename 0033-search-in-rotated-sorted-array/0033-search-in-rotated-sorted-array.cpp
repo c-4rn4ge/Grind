103d1b153b68f1bb6316d5a8d0ced7f7d64d0fd2
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-08 07:17:43 +0530

    Time: 4 ms (44.66%), Space: 11 MB (45.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,16 +8,14 @@ public:
         {
             int m = l + (h-l)/2;
             if(nums[m] == k) return m;
-            else if(nums[l]==k) return l;
-            else if(nums[h]==k) return h;
             else if(nums[l] <= nums[m])
             {
-                if(nums[l] < k && nums[m]>k) h = m-1;
+                if(nums[l] <= k && nums[m]>=k) h = m-1;
                 else l  = m+1;
             }
             else 
             {
-                if(nums[m] < k && nums[h] > k) l = m+1;
+                if(nums[m] <= k && nums[h] >= k) l = m+1;
                 else h = m-1;
             }
         }
