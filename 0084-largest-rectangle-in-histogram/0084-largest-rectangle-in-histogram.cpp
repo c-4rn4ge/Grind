82c2f5df098ca619745f5ff4cbfad321c5ec337c
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-08 09:14:19 +0530

    Time: 125 ms (96.44%), Space: 75.4 MB (90.72%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,54 +1,19 @@
 class Solution {
 public:
-    vector<int> nsl(vector<int> &h)
+    int largestRectangleArea(vector<int>& h) 
     {
-        int n = h.size();
-        stack<pair<int,int>> s;
-        vector<int> left;
-        for(int i = 0;i<n;i++)
+        stack<int> s;
+        int i,n=h.size(),ans=0;
+        for(i=0;i<=n;i++)
         {
-            if(!s.size()) left.push_back(-1);
-            else if(s.top().first < h[i])   left.push_back(s.top().second);
-            else
+            while(s.size() && (i == n || h[s.top()] >= h[i]))
             {
-                while(s.size() && s.top().first >= h[i]) s.pop();
-                (s.size()) ? left.push_back(s.top().second) : left.push_back(-1);
+                int a = h[s.top()];
+                s.pop();
+                int l = (!s.size()) ? -1 : s.top();
+                ans = max(ans,(i-l-1)*a);
             }
-            s.push({h[i],i});
-        }
-        return left;
-    }
-    vector<int> nsr(vector<int> &h)
-    {
-        int n = h.size();
-        stack<pair<int,int>> s;
-        vector<int> left;
-        for(int i = n-1;i>=0;i--)
-        {
-            if(!s.size()) left.push_back(n);
-            else if(s.top().first < h[i])   left.push_back(s.top().second);
-            else
-            {
-                while(s.size() && s.top().first >= h[i]) s.pop();
-                (s.size()) ? left.push_back(s.top().second) : left.push_back(n);
-            }
-            s.push({h[i],i});
-        }
-        reverse(left.begin(),left.end());
-        return left;
-    }
-    int largestRectangleArea(vector<int>& heights) 
-    {
-        vector<int> left = nsl(heights);
-        vector<int> right = nsr(heights);
-        for(auto i: left) cout<<i<<" ";
-        cout<<endl;
-        for(auto i: right) cout<<i<<" ";
-        cout<<endl;
-        int ans = 0;
-        for(int i = 0;i<heights.size();i++)
-        {
-            ans = max(ans,(right[i]-left[i]-1) * heights[i]);
+            s.push(i);
         }
         return ans;
     }
