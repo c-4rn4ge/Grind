3453d92b698aa70881778cf911ce5b37d2584e96
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-10 03:01:35 +0530

    Time: 3 ms (99.84%), Space: 9.1 MB (18.16%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) 
-    {
+    string removeKdigits(string num, int k) {
         stack<char> st;
-        if(k == num.size()) return "0";
-        for(int i = 0; i < num.size(); i++) 
-        {
-            while(!st.empty() && num[i] < st.top() && k ) 
-            {
-                st.pop();
+        if(k == num.size())
+            return "0";
+        
+        for(int i = 0; i < num.size(); i++) {
+            while(k > 0 && !st.empty() && num[i] < st.top()) {
                 k--;
+                st.pop();
             }
-            if(st.size() || num[i] != '0')st.push(num[i]);
+            st.push(num[i]);
         }
-        while(st.size() && k--) 
-        {
+        
+        while(k > 0) {
             st.pop();
+            k--;
         }
+        
         string ans;
-        while(!st.empty()) 
-        {
+        while(!st.empty()) {
             ans.push_back(st.top());
             st.pop();
         }
+        
         reverse(ans.begin(), ans.end());
-        return (ans.size()) ? ans : "0";
+        int ind;
+        
+        for(int i = 0; i < ans.size(); i++) {
+            if(ans[i] != '0') {
+                ind = i;
+                break;
+            }
+        }
+        
+        if(ans.substr(ind).size() == 0)
+            return "0";
+        
+        return ans.substr(ind);
     }
 };
