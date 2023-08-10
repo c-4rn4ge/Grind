c7c1099541cd434b1f4d1126f86002370e2bd164
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-10 07:32:36 +0530

    Time: 58 ms (99.23%), Space: 39.4 MB (91.62%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,47 +1,25 @@
 class Solution 
 {
 public:
-    int mod = 1e9+7;
-    vector<int> prevBound(vector<int> &arr) 
+    int mod=1e9+7;
+    int sumSubarrayMins(vector<int>& nums) 
     {
-        int n = arr.size();
-        vector<int> ans(n);
+        ios_base::sync_with_stdio(false);
+        int n=nums.size(),l;
         stack<int> st;
-        for(int i = 0;i < n; i++) 
+        long long ans=0;
+        for(int i=0;i<=n;i++)
         {
-            while(!st.empty() && arr[st.top()] >= arr[i])   st.pop();   
-            ans[i] = st.empty() ? -1 : st.top();
+            while(!st.empty() && ((i==n) || nums[st.top()]>nums[i]) )
+            {
+                int ind=st.top();
+                st.pop();
+                if(st.empty())  l=ind+1;
+                else            l=ind-st.top();
+                ans=(ans+(long long )nums[ind]*l*(i-ind))%mod;
+            }
             st.push(i);
         }
-        return ans;
-    }
-    vector<int> nextBound(vector<int> &arr) 
-    {
-        int n = arr.size();
-        vector<int> ans(n);
-        stack<int> st;
-        for(int i = n-1;i >= 0; i--) 
-        {
-            while(!st.empty() && arr[st.top()] > arr[i])    st.pop();
-            ans[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-
-        return ans;
-    }
-
-    int sumSubarrayMins(vector<int>& arr) 
-    {
-        vector<int> prevMin = prevBound(arr);
-        vector<int> nextMin = nextBound(arr);
-        long long int sum = 0;
-        for(int i = 0;i < arr.size(); i++) 
-        {
-            long long int leftMin = i-prevMin[i];
-            long long int rightMin = nextMin[i]-i;
-            sum = (sum+1ll*arr[i]*leftMin*rightMin)%mod;
-        }
-
-        return sum;
+        return ans%mod;
     }
 };
