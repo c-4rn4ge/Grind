89dcc87381768d112f8409d410e9d0cadac2de11
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-15 08:46:54 +0530

    Time: 0 ms (100%), Space: 10.2 MB (83.57%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -8,55 +8,59 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
+class Solution {
 public:
-    ListNode *solve(ListNode *head, int x)
+    ListNode* partition(ListNode* head, int x) 
     {
-        ListNode *s = nullptr, *l = nullptr, *fs = nullptr, *fl = nullptr;
-        int f = 0, f1 = 0;
-        while (head)
-        {
-            if (head->val < x)
+        if(head == NULL)    return head;
+        
+        ListNode *head1=NULL,*temp1,*head2=NULL,*temp2;
+        ListNode *curr=head;
+        
+        while(curr!=NULL){
+            if(curr->val<x)
             {
-                if (!f)
+                if(head1 == NULL)
                 {
-                    s = head;
-                    fs = s;
-                    f++;
+                    head1=curr;
+                    curr=curr->next;
+                    temp1=head1;
+                    
                 }
                 else
                 {
-                    s->next = head, s = head;
+                    temp1->next=curr;
+                    curr=curr->next;
+                    temp1=temp1->next;
                 }
-                head = head->next;
-                s->next = nullptr;
+                
             }
             else
             {
-                if (!f1)
+                if(head2 == NULL)
                 {
-                    l = head;
-                    fl = l;
-                    f1++;
+                    head2=curr;
+                    curr=curr->next;
+                    temp2=head2;
+                    temp2->next=NULL;
+                    
+                }else{
+                    temp2->next=curr;
+                    curr=curr->next;
+                    temp2=temp2->next;
+                    temp2->next=NULL;
                 }
-                else
-                {
-                    l->next = head, l = head;
-                }
-                head = head -> next;
-                l -> next = nullptr;
+                
             }
         }
-        if (fs)
-            s->next = fl;
-        else return fl;
-        return fs;
-    }
-    ListNode *partition(ListNode *head, int x)
-    {
-        if (!head || !head->next)
-            return head;
-        return solve(head, x);
+        if(head1!=NULL)
+        {
+            temp1->next = head2;
+            return head1;
+        }
+        else
+        {
+            return head2;
+        }
     }
 };
