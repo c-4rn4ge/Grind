025847b422b0f1e0d23778904720679e5672dddd
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-15 08:20:16 +0530

    Time: 8 ms (38.42%), Space: 10.2 MB (48.14%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *solve(ListNode *head, int x)
+    {
+        ListNode *s = nullptr, *l = nullptr, *fs = nullptr, *fl = nullptr;
+        int f = 0, f1 = 0;
+        while (head)
+        {
+            if (head->val < x)
+            {
+                if (!f)
+                {
+                    s = head;
+                    fs = s;
+                    f++;
+                }
+                else
+                {
+                    s->next = head, s = head;
+                }
+                head = head->next;
+                s->next = nullptr;
+            }
+            else
+            {
+                if (!f1)
+                {
+                    l = head;
+                    fl = l;
+                    f1++;
+                }
+                else
+                {
+                    l->next = head, l = head;
+                }
+                head = head -> next;
+                l -> next = nullptr;
+            }
+        }
+        if (fs)
+            s->next = fl;
+        else
+            return fl;
+        return fs;
+    }
+    ListNode *partition(ListNode *head, int x)
+    {
+        if (!head || !head->next)
+            return head;
+        return solve(head, x);
+    }
+};
