34194ccf01a2879f072b112b2798a83a70b7ec1f
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-18 16:16:02 +0530

    Time: 126 ms (96.88%), Space: 47.7 MB (75.33%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& edges, 
+    vector<int> &vis,vector<int> &pathVis,int node,int v)
+    {
+        vis[node] = 1;
+        pathVis[node] = 1;
+        for(auto &i : edges[node])
+        {
+            if(!vis[i])
+            {
+                if(dfs(edges,vis,pathVis,i,v)) return true;
+            }
+            else if(pathVis[i]) return true;
+        }
+        pathVis[node] = 0; 
+        return false;
+    }
+
+    bool isCyclic(vector<vector<int>>& edges, int v, int e)
+    {
+        vector<vector<int>> ans(v);
+        for(int i = 0;i<edges.size();i++)
+        {
+            int e1 = edges[i][0];
+            int e2 = edges[i][1];
+            ans[e1].push_back(e2);
+        }
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
+    {
+        int v = graph.size();
+        vector<int> vis(v,0),pathVis(v,0);
+        for(int i = 0 ; i < v ; i++)
+        {
+            if(!vis[i])
+            {
+                dfs(graph,vis,pathVis,i,v);
+            }
+        }
+        vector<int> ans;
+        for(int i = 0 ; i < v ; i++)
+        {
+            if(!pathVis[i]) ans.push_back(i);
+        }     
+        return ans;
+    }
+};
