fd822553ed580bf9d2543b1175ae710219fe5e23
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-20 18:36:13 +0530

    Time: 81 ms (88.08%), Space: 44.6 MB (78.49%) - LeetHub

---
+++ 1309-sort-items-by-groups-respecting-dependencies/1309-sort-items-by-groups-respecting-dependencies.cpp
@@ -0,0 +1,73 @@
+class Solution
+{
+public:
+    vector<int> topologicalSort(vector<vector<int>> &adjacency, vector<int> &degrees, int size)
+    {
+        queue<int> q;
+        for (int i = 0; i < size; ++i)
+            if (degrees[i] == 0)
+                q.push(i);
+        vector<int> result;
+        while (!q.empty())
+        {
+            int x = q.front();
+            q.pop();
+            result.push_back(x);
+            for (int y : adjacency[x])
+            {
+                degrees[y]--;
+                if (degrees[y] == 0)
+                    q.push(y);
+            }
+        }
+        for (int i = 0; i < size; ++i)
+            if (degrees[i] > 0)
+                return {};
+        return result;
+    }
+
+    vector<int> sortItems(int numItems, int numGroups, vector<int> &itemGroups, vector<vector<int>> &beforeItems)
+    {
+        for (int i = 0; i < numItems; ++i)
+        {
+            if (itemGroups[i] == -1)
+                itemGroups[i] = numGroups++;
+        }
+        vector<vector<int>> groupAdjacency(numGroups), itemAdjacency(numItems);
+        vector<int> groupDegrees(numGroups, 0), itemDegrees(numItems, 0);
+        for (int i = 0; i < numItems; ++i)
+        {
+            int toItemGroup = itemGroups[i];
+            for (int fromItem : beforeItems[i])
+            {
+                int fromItemGroup = itemGroups[fromItem];
+                if (toItemGroup != fromItemGroup)
+                {
+                    groupAdjacency[fromItemGroup].push_back(toItemGroup);
+                    groupDegrees[toItemGroup]++;
+                }
+                itemAdjacency[fromItem].push_back(i);
+                itemDegrees[i]++;
+            }
+        }
+        vector<int> sortedGroups = topologicalSort(groupAdjacency, groupDegrees, numGroups);
+        vector<int> sortedItems = topologicalSort(itemAdjacency, itemDegrees, numItems);
+        if (sortedGroups.empty() || sortedItems.empty())
+            return {};
+
+        vector<vector<int>> itemsInGroup(numGroups);
+        for (int i : sortedItems)
+            itemsInGroup[itemGroups[i]].push_back(i);
+
+        vector<int> answer;
+        for (int i : sortedGroups)
+        {
+            for (int item : itemsInGroup[i])
+            {
+                answer.push_back(item);
+            }
+        }
+
+        return answer;
+    }
+};
