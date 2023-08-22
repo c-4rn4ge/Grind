25f92ebcdb35d56de847212d0f4f25744e8ee1c3
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-22 16:17:18 +0530

    Time: 7 ms (84.63%), Space: 10.1 MB (16.41%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -14,7 +14,22 @@ public:
     {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,grid,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        // return solve(m-1,n-1,grid,dp);
+        for(int i = 0 ; i < m ; i++)
+        {
+            for(int j = 0 ; j < n ; j++)
+            {
+                if(i == 0 && j == 0) dp[0][0] = grid[0][0];
+                else
+                {
+                    int u = INT_MAX, l = INT_MAX;
+                    if(i>0) u = dp[i-1][j] + grid[i][j];
+                    if(j>0) l = dp[i][j-1] + grid[i][j];
+                    dp[i][j] = min(u,l);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
