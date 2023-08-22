e9fda8632b29524cb7fc616b5c00fa00d87ac265
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-22 17:18:36 +0530

    Time: 4 ms (83.11%), Space: 9 MB (21.17%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,7 +13,17 @@ public:
     {
         int m = triangle.size();
         int n = triangle[m-1].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(0,0,m,triangle,dp);
+        vector<vector<int>> dp(m,vector<int>(m,0));
+        for(int i = 0 ; i < n ; i++)    dp[m-1][i] = triangle[m-1][i];
+        for(int i = m-2 ; i >= 0 ; i--)
+        {
+            for(int j = i ; j >= 0 ; j--)
+            {
+                int d  = triangle[i][j] + dp[i+1][j] ;
+                int dg = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(d,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
