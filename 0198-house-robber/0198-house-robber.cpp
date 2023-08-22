750d57793591e32931178b6f4062aaf9b0dccaea
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-22 14:19:39 +0530

    Time: 4 ms (27.35%), Space: 7.8 MB (46.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,4 +1,5 @@
-class Solution {
+class Solution 
+{
 public:
     int solve(int n,vector<int> & nums, vector<int> &dp)
     {
@@ -11,7 +12,16 @@ public:
     }
     int rob(vector<int>& nums) 
     {
-        vector<int> dp(nums.size(),-1);
-        return solve(nums.size()-1,nums,dp);
+        vector<int> dp(nums.size(),0);
+        dp[0] = nums[0];
+        int n = nums.size();
+        for(int i = 1; i < n ; i++)
+        {
+            int left = 0,right = 0;
+            (i > 1) ? left  = dp[i-2] + nums[i] : left = nums[i];
+            right = dp[i-1];
+            dp[i] = max(left,right);
+        }
+        return dp[n-1];
     }
 };
