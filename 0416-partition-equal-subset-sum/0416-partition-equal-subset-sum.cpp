d580ded0d8f96f38cd6e278445f5971e9ef7fae1
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-22 20:21:36 +0530

    Time: 133 ms (88.67%), Space: 11.9 MB (79.32%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,22 @@
-class Solution 
-{
+class Solution {
 public:
-    bool subsetSumToK(int n, int k, vector < int > & arr) 
-    {
-        vector < vector < bool >> dp(n, vector < bool > (k + 1));
-        for(int i = 0 ; i < n ; i++)    dp[i][0] = true;
-        for(int i = 0 ; i <= k ; i++) if(arr[0] == i) dp[0][i] = true;
-        for(int i = 0 ; i < n ; i++)
-        {
-            for(int j = 1; j <= k ; j++)
-            {
-                if(i>0)dp[i][j] = dp[i-1][j];
-                if(j - arr[i] >= 0 && i>0) dp[i][j] = dp[i][j] || dp[i-1][j-arr[i]];
-            }
-        }
-        return dp[n-1][k];
-    }
     bool canPartition(vector<int>& nums) 
     {
-        int sum = 0;
-        for(auto &i : nums) sum += i;
-        if(sum & 1) return false;
-        return subsetSumToK(nums.size(),sum/2,nums);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum%2 !=0 ) return false;
+        int n = nums.size();
+        sum = sum/2;
+        bool possible[n+1][sum+1];
+        for(int i=1; i<=sum; i++) possible[0][i] = false;
+        for(int i=0; i<=n; i++) possible[i][0] = true;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=sum; j++)
+            {
+                if(nums[i-1] > j) possible[i][j] = possible[i-1][j];
+                else possible[i][j] = possible[i-1][j-nums[i-1]] || possible[i-1][j];
+            }
+        }
+        return possible[n][sum];
     }
 };
