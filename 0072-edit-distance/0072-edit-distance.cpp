bd684e73727a9a7e751b009bf2f12cc0c9eeeff5
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-25 21:59:06 +0530

    Time: 10 ms (73.96%), Space: 9 MB (40.37%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,5 @@
 class Solution 
 {
-    int solve(int i , int j , string &s , string &t , vector<vector<int>> &dp)
-    {
-        if(i==0) return j;
-        if(j==0) return i;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i-1] == t[j-1])
-        {
-            return dp[i][j] = solve(i-1,j-1,s,t,dp);
-        }
-        else
-        {
-            int rep = solve(i-1,j-1,s,t,dp);
-            int del = solve(i-1,j,s,t,dp);
-            int ins = solve(i,j-1,s,t,dp);
-            return dp[i][j] = 1 + min(rep , min(del , ins));
-        }
-    }
 public:
     int minDistance(string s, string t) 
     {
