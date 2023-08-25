c9093e23d5586af13a2f2c2280073fc218c4ecb2
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-25 22:01:21 +0530

    Time: 4 ms (95.54%), Space: 6.4 MB (93.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,24 @@
-class Solution 
+class Solution
 {
-    int solve(int i , int j , string &s , string &t , vector<vector<int>> &dp)
-    {
-        if(i==0) return j;
-        if(j==0) return i;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i-1] == t[j-1])
-        {
-            return dp[i][j] = solve(i-1,j-1,s,t,dp);
-        }
-        else
-        {
-            int rep = solve(i-1,j-1,s,t,dp);
-            int del = solve(i-1,j,s,t,dp);
-            int ins = solve(i,j-1,s,t,dp);
-            return dp[i][j] = 1 + min(rep , min(del , ins));
-        }
-    }
 public:
-    int minDistance(string s, string t) 
+    int minDistance(string s, string t)
     {
-        int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,s,t,dp);
+        int m = s.size(), n = t.size();
+        vector<int> p(n + 1, 0), c(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+            p[i] = i;
+        for (int i = 1; i <= m; i++)
+        {
+            c[0] = i;
+            for (int j = 1; j <= n; j++)
+            {
+                if (s[i - 1] == t[j - 1])
+                    c[j] = p[j - 1];
+                else
+                    c[j] = min(p[j - 1], min(c[j - 1], p[j])) + 1;
+            }
+            p = c;
+        }
+        return p[n];
     }
 };
