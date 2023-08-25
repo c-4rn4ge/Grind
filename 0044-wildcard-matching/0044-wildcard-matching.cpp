1b13e505f038f4b95c7cbec2e51d161163061a41
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-25 22:17:53 +0530

    Time: 32 ms (91.49%), Space: 27.9 MB (13.45%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
+    int solve(int i , int j , string &s , string &t, vector<vector<int>> &dp)
+    {
+        if(i== 0 && j == 0)
+        {
+            return 1;
+        }
+        if(j==0) return 0;
+        if(i==0)
+        {
+            for(int a = 1; a <= j; a++) 
+            {
+                if(t[a-1] != '*') return 0;
+            }
+            return 1;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s[i-1] == t[j-1] || t[j-1] == '?')
+        {
+            return dp[i][j] = solve(i-1,j-1,s,t,dp);
+        }
+        else if(t[j-1] == '*')
+        {
+            return dp[i][j] = solve(i,j-1,s,t,dp) || solve(i-1,j,s,t,dp);
+        }
+        return 0;
+    }
     bool isMatch(string s, string p) 
     {
-        int n = s.size(), m = p.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1));
-        dp[0][0] = true;
-        for(int k=1; k<=m; k++) 
-        {
-            if(p[k - 1] == '*') dp[0][k] = true;
-            else break;
-        }
-        for(int i=1; i<=n; i++) 
-        {
-            for(int j=1; j<=m; j++) 
-            {
-                if(s[i - 1] == p[j - 1] || p[j - 1] == '?') dp[i][j] = dp[i - 1][j - 1];
-                else if(p[j - 1] == '*')     dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
-            }
-        }
-        return dp[n][m];
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(n,m,s,p,dp);
     }
 };
