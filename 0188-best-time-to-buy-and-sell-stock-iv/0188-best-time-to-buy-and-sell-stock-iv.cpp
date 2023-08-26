01d10cf3fec675012dae2abc0b5ba1263c1a3ac3
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-27 00:09:54 +0530

    Time: 8 ms (89.72%), Space: 14.4 MB (45.11%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(int how, vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(how+1,0)));
+        for(int i = n-1 ; i >= 0 ; i--)
+        {
+            for(int j = 0 ; j <= 1 ; j++)
+            {
+                for(int k = 1 ; k <= how ; k++)
+                {
+                    if(j == 1)
+                    {   
+                        dp[i][j][k] = max(-nums[i] + dp[i+1][0][k] , dp[i+1][1][k]);
+                    }
+                    else 
+                    {
+                        dp[i][j][k] = max(nums[i] + dp[i+1][1][k-1] , dp[i+1][0][k]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][how];    
+    }
+};
