b98eab1304b91e2bc3805a325d930de578153d79
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-08-30 04:30:05 +0530

    Time: 163 ms (80.35%), Space: 58.8 MB (49.2%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,48 +1,31 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& edges, vector<int> &vis,vector<int> &pathVis,int node,int v)
-    {
-        vis[node] = 1;
-        pathVis[node] = 1;
-        for(auto &i : edges[node])
-        {
-            if(!vis[i])
-            {
-                if(dfs(edges,vis,pathVis,i,v)) return true;
-            }
-            else if(pathVis[i]) return true;
-        }
-        pathVis[node] = 0; 
-        return false;
-    }
-
-    bool isCyclic(vector<vector<int>>& edges, int v, int e)
-    {
-        vector<vector<int>> ans(v);
-        for(int i = 0;i<edges.size();i++)
-        {
-            int e1 = edges[i][0];
-            int e2 = edges[i][1];
-            ans[e1].push_back(e2);
-        }
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
     {
-        int v = graph.size();
-        vector<int> vis(v,0),pathVis(v,0);
-        for(int i = 0 ; i < v ; i++)
+        int n = graph.size();
+        vector<int> adj[n],in(n,0),ans;
+        queue<int> que;
+        for(int i = 0 ; i < n; i++)
         {
-            if(!vis[i])
+            for(auto &j : graph[i])
             {
-                dfs(graph,vis,pathVis,i,v);
+                adj[j].push_back(i);
+                in[i]++;
             }
         }
-        vector<int> ans;
-        for(int i = 0 ; i < v ; i++)
+        for(int i = 0 ; i < n ; i++) if(!in[i]) que.push(i);
+        while(que.size())
         {
-            if(!pathVis[i]) ans.push_back(i);
-        }     
+            int cur = que.front();
+            que.pop();
+            ans.push_back(cur);
+            for(auto &i : adj[cur])
+            {
+                in[i]--;
+                if(!in[i]) que.push(i); 
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
