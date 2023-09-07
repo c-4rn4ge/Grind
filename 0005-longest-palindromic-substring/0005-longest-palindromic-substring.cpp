45afb5e77b5cdf9d384bf5e079594edde280a26d
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-09-07 17:07:51 +0530

    Time: 2 ms (99.37%), Space: 6.9 MB (76.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,36 +1,26 @@
 class Solution 
 {
-private:
-    int solve(int i, int j, string s) 
-    {
-        int l = i;
-        int r = j;
-        while (l >= 0 && r < s.length() && s[l] == s[r])  l--,r++;
-        return r - l - 1;
-    }
 public:
     string longestPalindrome(string s) 
     {
-        int row = 0,col=0;
-        for (int i = 0; i < s.length(); i++) 
+        int n = s.length();
+        if(n==0) return "";
+        if(n==1) return s;
+        int start = 0, end = 0,i=0;
+        while(i<n)
         {
-            int odd = solve(i, i, s);
-            if (odd > col - row + 1) 
+            if(n-i < end/2) break;
+            int l = i, r = i;
+            while(r < n && s[r] == s[r + 1])    r++;
+            i = r + 1;
+            while(l > 0 && r < n && s[l-1] == s[r+1])   l--,r++;
+            int newlen = r - l + 1;
+            if(newlen > end)
             {
-                int dist = odd / 2;
-                row = i - dist;
-                col = i + dist;
-            }
-            
-            int even = solve(i, i + 1, s);
-            if (even > col - row + 1) 
-            {
-                int dist = (even / 2) - 1;
-                row = i - dist;
-                col = i + 1 + dist;
+                end = newlen;
+                start = l;
             }
         }
-
-        return s.substr(row, col-row+1);
+        return s.substr(start,end);   
     }
 };
