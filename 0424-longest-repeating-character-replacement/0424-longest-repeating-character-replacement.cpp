b0ae4c2104561b60f66918652f5644351032f5a6
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-09-08 03:31:49 +0530

    Time: 3 ms (98.2%), Space: 6.9 MB (98.89%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,13 +1,14 @@
-class Solution {
+class Solution 
+{
 public:
     int characterReplacement(string s, int k) 
     {
-        unordered_map<char,int> map;    
+        int map[26] = {0};    
         int maxf=0,ans=0,l=0;
         for(int i=0;i<s.length();i++)
         {
-            maxf = max(maxf,++map[s[i]]);
-            if( (i - l + 1) - maxf >  k)    map[s[l++]]--;
+            maxf = max(maxf,++map[s[i]-'A']);
+            if((i - l + 1) - maxf >  k)    map[s[l++]-'A']--;
             ans = max(ans,i-l+1);
         }
         return ans;
