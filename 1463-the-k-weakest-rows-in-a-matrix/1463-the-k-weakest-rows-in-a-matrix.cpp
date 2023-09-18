878a3231a278d7acb781fd8226108010bff38103
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-09-18 08:14:01 +0530

    Time: 8 ms (76.19%), Space: 10.9 MB (9.15%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,30 +1,24 @@
-class Solution {
+class Solution 
+{
 public:
-    int bs(vector<int> &a)
-    {
-        int l = 0,h = a.size()-1,m;
-        while(l<=h)
-        {
-            m = l + (h-l)/2;
-            if(a[m]==1) l = m+1;
-            else
-            {
-                if(a[l]==0) return l;
-                l++;
-                h = m;
-            }
-        }
-        return a.size();
-    }   
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
     {
-        vector<pair<int,int>> m;
+        int n= mat.size();
+        int m = mat[0].size();
         vector<int> ans;
-        for(int i=0;i<mat.size();i++)  
-            m.push_back({bs(mat[i]),i});
-        sort(m.begin(),m.end());
-        for(int i = 0; i<k ;i++)       
-            ans.push_back(m[i].second);
+        vector<pair<int,int>> v;
+        for(int i = 0;i<n;i++)
+        {
+            int val = lower_bound(mat[i].begin(),
+            mat[i].end(),0,greater<int>())-mat[i].begin();
+            v.push_back({val,i});
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<k;i++)
+        {
+            ans.push_back(v[i].second);
+        }
         return ans;
+
     }
 };
