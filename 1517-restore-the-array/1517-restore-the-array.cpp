697247da6ee8a5f4ae5f6410f95e18ff2a0e57c0
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-09-19 14:57:28 +0530

    Time: 55 ms (60.34%), Space: 95.2 MB (5.07%) - LeetHub

---
+++ 1517-restore-the-array/1517-restore-the-array.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    long long mod = 1000000007;
+    long long solve(string &s , long long k ,long long i, vector<long long> &dp)
+    {
+        if(i == s.length()) return 1;
+        if(dp[i] != -1) return dp[i];
+        long long sum = 0,curr = 0;
+        for(long long j = i; j < s.length(); j++)
+        {
+            sum = (sum * 10 + (s[j] - '0'));
+            if(sum >=1 && sum <= k) curr = (curr + solve(s,k,j+1,dp)) % mod;
+            else break;
+        }
+        return dp[i] = curr;
+    }
+    int numberOfArrays(string s, int k) 
+    {
+        vector<long long> dp(100001,-1);
+        return (int)solve(s,k,0,dp); 
+    }
+};
