b975e07711b1ea29415e1484a1cc8f14981dc1f8
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-09-19 09:56:45 +0530

    Time: 3 ms (99.34%), Space: 8.2 MB (28.79%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,40 +1,20 @@
 class Solution 
 {
-private:
-    vector<int> ht;
-    vector<int> st;
 public:
-    Solution()
-    {
-       ht.resize(123);
-       st.resize(123);
-    }
-    bool check()
-    {
-        for(int i=65;i<123;i++)
-        {
-            if(ht[i] != 0 && ht[i] > st[i]) return false;
-        }   
-        return true;
-    }
     string minWindow(string s, string t) 
     {
-        int l = 0,r=0,sl=s.size(),tl = t.length();
-        string ans = "";
-        int c = INT_MAX;
-        if(s.size() < t.size()) return "";
-        for(int i=0;i<tl;i++)   ht[t[i]]++;
-        while(r < sl)
+        vector<int> h(128,0);
+        for(auto &i: t) h[i]++;
+        int c = t.size(), j = 0, i = 0, end = INT_MAX, start = 0,n = s.length();
+        while(i < n)
         {
-            st[s[r]]++;
-            while(l <= r && (ht[s[l]] == 0 || st[s[l]] > ht[s[l]])) st[s[l++]]--;
-            if(check())
-            {
-                if(c > r - l + 1)   ans = s.substr(l,c = r - l + 1);
-                st[s[l++]]--;
-            }
-            r++;
+            if(h[s[i++]]-- > 0) c--; 
+            while(c==0)
+            { 
+                if(i-j<end)  end = i - (start = j);
+                if(h[s[j++]]++ == 0) c++;  
+            }  
         }
-        return ans;
+        return (end == INT_MAX) ?  "" : s.substr(start, end);
     }
 };
