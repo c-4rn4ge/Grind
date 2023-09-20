1e3adddeedf45fef056ce88b34b2c01a1a8227ab
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-09-20 20:09:51 +0530

    Time: 4 ms (36.7%), Space: 6.3 MB (47.7%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,21 +1,15 @@
 class Solution 
 {
-private:
-    long long solve(long long n)
-    {
-        if(n <= 1) return 0;
-        long long left = INT_MAX,ch1 = INT_MAX,ch2 = INT_MAX;
-        if(!(n % 2)) left = 1 + solve(n/2);
-        else
-        {
-            ch1 = 1 + solve(n-1);
-            ch2 = 1 + solve(n+1);
-        }
-        return min(left,min(ch1,ch2));
-    }
 public:
+    int solve(long n, int ans)
+    {
+        if (n <= 1) return ans;
+        if (!(n%2))     return solve(n/2, ans+1); 
+        else            return min(solve(n+1, ans+1), solve(n-1, ans+1));
+    }
+
     int integerReplacement(int n) 
     {
-        return (int)solve((long long)n);
+        return solve(n, 0);
     }
 };
