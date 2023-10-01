1f49c7dfeb713ca8532a2c56c91408f495d68c73
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-10-01 22:01:35 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
         unordered_map<int, int> mp;
-
         for(int i = 0; i < nums.size(); i++){
             if(mp.find(target - nums[i]) == mp.end())
                 mp[nums[i]] = i;
