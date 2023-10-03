791d9476f36a95390b2521f058121303bf7be654
Devansh Rathod <133647358+dr-sync@users.noreply.github.com>
2023-10-03 13:20:26 +0530

    Time: 103 ms (45.36%), Space: 72 MB (13.72%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(begin(nums),end(nums));
-        for(int i = 0; i < nums.size() - 1; i++)
-        {
-            if(nums[i] == nums[i+1]) return true;
-        }
-        return false;
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        unordered_set<int> st(nums.begin(),nums.end());
+        return !(st.size() == nums.size());
     }
 };
