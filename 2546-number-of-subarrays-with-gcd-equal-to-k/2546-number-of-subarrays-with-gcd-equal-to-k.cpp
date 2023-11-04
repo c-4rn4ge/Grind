55727c13120b516364ed62d26294b0e729f7ea01
Devansh Rathod <133647358+eX-orcist@users.noreply.github.com>
2023-11-04 13:37:57 +0530

    Time: 21 ms (44.57%), Space: 9.2 MB (68.52%) - LeetHub

--- 2546-number-of-subarrays-with-gcd-equal-to-k/2546-number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/2546-number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -3,16 +3,11 @@ class Solution
 public:
     int subarrayGCD(vector<int>& nums, int k) 
     {
-        int ans = 0, n = nums.size();
+        int ans = 0, n = nums.size(), x;
         for(int i = 0; i < n ; i++)
         {
-            if(nums[i] == k) ans++;
-            int x = nums[i];
-            for(int j = i + 1; j < n; j++)
-            {
-                x = __gcd(x, nums[j]);
-                if(x == k)  ans++;
-            }
+            if((x = nums[i]) == k) ans++;
+            for(int j = i + 1; j < n; j++)  if((x = __gcd(x, nums[j])) == k)  ans++;
         }
         return ans;
     }
