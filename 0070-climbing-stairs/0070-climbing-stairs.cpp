3e36e3edb58400c4c0a920d92450a7bcbe2bbcbf
Devansh Rathod <133647358+eX-orcist@users.noreply.github.com>
2023-11-04 14:21:10 +0530

    Time: 2 ms (46.73%), Space: 6.2 MB (75.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,9 +3,8 @@ class Solution
 public:
     int climbStairs(int n) 
     {
-        int a ,b;
-        a = 0; b = 1;
-        for(int i = 1; i <= n; i++)   
+        int a = 0,b = 1;
+        for(int i = 0; i < n; i++)   
         {
             int c = a + b;      
             a = b;
