e2ad5a43129bb462dd6275f1b0a2e7f14f9f5797
Devansh Rathod <133647358+eX-orcist@users.noreply.github.com>
2023-11-04 14:21:58 +0530

    Time: 2 ms (46.73%), Space: 6.3 MB (58.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,9 +6,8 @@ public:
         int a = 0,b = 1;
         for(int i = 0; i < n; i++)   
         {
-            int c = a + b;      
-            a = b;
-            b = c;
+            int c = a + b;
+            a = b,b = c;
         }
         return b;
     }
