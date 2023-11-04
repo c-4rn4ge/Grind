20e7f051f2fabccb502a05f077e494e5a2e2a1a6
Devansh Rathod <133647358+eX-orcist@users.noreply.github.com>
2023-11-04 14:11:41 +0530

    Time: 0 ms (100%), Space: 6.5 MB (26.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,14 @@
-class Solution 
-{
-private:
-    int solve(int i, vector<int> &dp)
-    {
-        if(i==0) return 1;
-        else if(i < 0) return 0;
-        if(dp[i] != -1 ) return dp[i];
-        return dp[i] = solve(i-1,dp) + solve(i-2,dp);
-    }
+class Solution {
 public:
+    int solve(int n,vector<int> &h)
+    {
+        if(n == 0 || n == 1) return 1;
+        if(h[n]!=-1) return h[n];
+        return h[n] = solve(n-1,h) + solve(n-2,h);
+    }
     int climbStairs(int n) 
     {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);        
+        vector<int> h(n+1,-1);
+        return solve(n,h);
     }
 };
